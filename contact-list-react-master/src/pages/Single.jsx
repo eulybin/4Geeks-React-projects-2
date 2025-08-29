bee2e998937f71933169dc4260e43369593dd16b
Egor Ulybin <egorulybin@gmail.com>
2025-08-29 20:00:41 +0200

    modified comments and whole proj structure

--- contact-list-react-master/src/pages/Single.jsx
+++ contact-list-react-master/src/pages/Single.jsx
@@ -1,27 +1,18 @@
-// Import necessary hooks and components from react-router-dom and other libraries.
-import { Link, useParams } from "react-router-dom";  // To use link for navigation and useParams to get URL parameters
-import PropTypes from "prop-types";  // To define prop types for this component
-import rigoImageUrl from "../assets/img/rigo-baby.jpg"  // Import an image asset
-import useGlobalReducer from "../hooks/useGlobalReducer";  // Import a custom hook for accessing the global state
+import { Link, useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import useGlobalReducer from '../hooks/useGlobalReducer';
 
-// Define and export the Single component which displays individual item details.
-export const Single = props => {
-  // Access the global state using the custom hook.
-  const { store } = useGlobalReducer()
-
-  // Retrieve the 'theId' URL parameter using useParams hook.
-  const { theId } = useParams()
-  const singleTodo = store.todos.find(todo => todo.id === parseInt(theId));
+export const Single = (props) => {
+  const { store } = useGlobalReducer();
+  const { theId } = useParams();
+  const singleTodo = store.todos.find((todo) => todo.id === parseInt(theId));
 
   return (
-    <div className="container text-center">
-      {/* Display the title of the todo element dynamically retrieved from the store using theId. */}
-      <h1 className="display-4">Todo: {singleTodo?.title}</h1>
-      <hr className="my-4" />  {/* A horizontal rule for visual separation. */}
-
-      {/* A Link component acts as an anchor tag but is used for client-side routing to prevent page reloads. */}
-      <Link to="/">
-        <span className="btn btn-primary btn-lg" href="#" role="button">
+    <div className='container text-center'>
+      <h1 className='display-4'>Todo: {singleTodo?.title}</h1>
+      <hr className='my-4' />
+      <Link to='/'>
+        <span className='btn btn-primary btn-lg' href='#' role='button'>
           Back home
         </span>
       </Link>
@@ -29,9 +20,7 @@ export const Single = props => {
   );
 };
 
-// Use PropTypes to validate the props passed to this component, ensuring reliable behavior.
+//maybe delete later
 Single.propTypes = {
-  // Although 'match' prop is defined here, it is not used in the component.
-  // Consider removing or using it as needed.
-  match: PropTypes.object
+  match: PropTypes.object,
 };
--- contact-list-react-master/src/store.js
+++ contact-list-react-master/src/store.js
@@ -1,32 +1,29 @@
-export const initialStore=()=>{
-  return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
-  }
-}
+export const initialStore = {
+  message: null,
+  todos: [
+    {
+      id: 1,
+      title: 'Go to the gym',
+      background: null,
+    },
+    {
+      id: 2,
+      title: 'Do my homework',
+      background: null,
+    },
+  ],
+};
 
-export default function storeReducer(store, action = {}) {
-  switch(action.type){
+export const storeReducer = (state, action = {}) => {
+  switch (action.type) {
     case 'add_task':
-
-      const { id,  color } = action.payload
+      const { id, color } = action.payload;
 
       return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        ...state,
+        todos: state.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo)),
       };
     default:
       throw Error('Unknown action.');
-  }    
-}
+  }
+};
