4d8541ea9a109a4c83633bd541b6fe4ce30e9121
Egor Ulybin <egorulybin@gmail.com>
2025-09-05 17:59:05 +0200

    formatted phone number before displaying on contact card

--- contact-list-react-master/src/components/ContactCard.jsx
+++ contact-list-react-master/src/components/ContactCard.jsx
@@ -6,6 +6,8 @@ const ContactCard = ({ name, address, phone, email }) => {
   const { theId } = useParams();
   //   const singleTodo = store.todos.find((todo) => todo.id === parseInt(theId));
 
+  const formattedPhoneNumber = `${phone.slice(0, 3)}-${phone.slice(3, 6)}-${phone.slice(6, 9)}`;
+
   return (
     <div className='container border border-black'>
       <div className='row'>
@@ -23,7 +25,7 @@ const ContactCard = ({ name, address, phone, email }) => {
             <p>
               <i className='fa-solid fa-phone' style={{ color: '#565656' }}></i>
               &nbsp;
-              {phone}
+              {formattedPhoneNumber}
             </p>
             <p>
               <i className='fa-solid fa-envelope' style={{ color: '#565656' }}></i>
@@ -34,10 +36,10 @@ const ContactCard = ({ name, address, phone, email }) => {
         </div>
         <div className='col-lg-3 d-flex mt-5 justify-content-center gap-5'>
           <div>
-            <i className='fa-solid fa-pen-to-square fs-5'></i>
+            <i className='fa-solid fa-pen-to-square fs-4'></i>
           </div>
           <div>
-            <i className='fa-solid fa-trash fs-5'></i>
+            <i className='fa-solid fa-trash fs-4'></i>
           </div>
         </div>
       </div>
